e60e39c0608dceab7636ee1a61c370ad00d2bfe2
saptarshi1996 <saptarshisg96@gmail.com>
2021-12-25 01:58:21 +0530

    remove dist

--- server/dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.App = void 0;
-class App {
-}
-exports.App = App;
-//# sourceMappingURL=app.js.map
--- server/dist/index.js
+++
@@ -1,2 +0,0 @@
-console.log("hello");
-//# sourceMappingURL=index.js.map
