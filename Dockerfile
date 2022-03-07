8dadbaf6b9c89fd0e26eb71ba6eff5aa59576298
saptarshi1996 <saptarshisg96@gmail.com>
2022-03-07 09:03:18 +0530

    client setup

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,14 @@
 FROM node:14.17.3
 
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . ./
+
+EXPOSE 8000 3306
+
+# RUN npx prisma db push
+RUN npm run build
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,4 +1,7 @@
 import { Request, ResponseToolkit, ResponseObject } from '@hapi/hapi'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 import { success, error } from '../helpers'
 import { IUser } from '../interfaces'
@@ -6,9 +9,23 @@ import { IUser } from '../interfaces'
 export const getUserDetails = async (req: Request, h: ResponseToolkit): Promise<ResponseObject> => {
   try {
 
-    const user = req.user as IUser
-    console.log(user)
-    return success(h, '')
+    const { id } = req.user as IUser
+
+    const userExists = await prisma.user.findFirst({
+      where: {
+        id
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        email: true,
+        isActive: true,
+      }
+    }) as IUser
+
+    return success(h, '', {
+      'user': userExists,
+    })
 
   } catch (ex) {
     return error(h, 'SERVER500', ex)
