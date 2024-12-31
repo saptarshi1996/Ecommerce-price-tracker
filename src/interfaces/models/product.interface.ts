daf6e99d1a644845a1a4f94cb359b7b3a150cad2
Saptarshi Sengupta <saptarshisengupta@Saptarshis-MacBook-Air.local>
2025-01-01 04:44:33 +0530

    product interface

---
+++ src/interfaces/models/product.interface.ts
@@ -0,0 +1,11 @@
+export default interface IProduct {
+  id?: number
+  name?: string
+  full_name?: string
+  url?: string
+  current_price?: number
+  lowest_price?: number
+  created_at?: Date
+  updated_at?: Date
+  deleted_at?: Date
+}
