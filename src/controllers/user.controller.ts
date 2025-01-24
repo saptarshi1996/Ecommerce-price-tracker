1bbf255b41a841c7e2ca1146898b515b577a762e
saptarshi1996 <saptarshisg96@gmail.com>
2025-01-24 16:51:32 +0530

    get user api

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,31 @@
+import { Request } from 'express'
+
+import {
+  getUser
+} from '../dao/user.dao'
+import NotFoundError from '../errors/notfound.error'
+
+export const fetchUser = async (req: Request) => {
+  const { id } = req.user
+
+  const userExists = await getUser({
+    where: {
+      id
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      email: true
+
+    }
+  })
+
+  if (!userExists) {
+    throw new NotFoundError('User does not exists')
+  }
+
+  return {
+    user: userExists
+  }
+}
